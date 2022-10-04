00b9cb92102a471c7051a29ae69ff7550857f1cd
Azizbek Ochilov <99602759+azizbek-ochilov@users.noreply.github.com>
2022-10-04 19:07:42 +0500

    Upload Project

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
 ﻿using System;
 System.Console.WriteLine("Please, enter first number:");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter operation(+ - * / %)");
+string operation = Console.ReadLine();
 System.Console.WriteLine("Please, enter second number:");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
+switch(operation)
+{
+    case "+":
+        System.Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber+secondNumber} ");
+        break;
+    case "-":
+        System.Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber-secondNumber} ");
+        break;
+    case "*":
+        System.Console.WriteLine($"{firstNumber} x {secondNumber} = {firstNumber*secondNumber} ");
+        break;
+    case "/":
+        System.Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber/secondNumber} ");
+        break;
+    case "%":
+        System.Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber%secondNumber} ");
+        break;
+
+}
 
 if(firstNumber>secondNumber)
 {
@@ -26,5 +47,22 @@ string massage1 =
     secondNumber >=0
         ? "2nd number is positive."
         : "2nd number is negative.";
+string massage2 = 
+    !(firstNumber >= 0) 
+        ? "1st number is not positive."
+        : "1st number is not negative.";
+string massage3 = 
+    firstNumber >= 0 && secondNumber>=0 
+        ? "1st and 2nd numbers are positive."
+        : "There is a negative in the entered number";
+string massage4 = 
+    firstNumber >= 0 || secondNumber>=0
+        ? "There is a positive in the entered number."
+        : "1st and 2nd numbers are negative";
+
+
 System.Console.WriteLine(massage);
 System.Console.WriteLine(massage1);
+System.Console.WriteLine(massage2);
+System.Console.WriteLine(massage3);
+System.Console.WriteLine(massage4);
