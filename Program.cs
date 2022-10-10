8003490f8f53d82be1d2cfe7c2ffafc6dfe952ad
Azizbek Ochilov <99602759+azizbek-ochilov@users.noreply.github.com>
2022-10-10 18:38:24 +0500

    Upload Project

--- Program.cs
+++ Program.cs
@@ -4,14 +4,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Security security = new Security();
-        Calculator calculator = new Calculator();
-        Projects project = new Projects();
-        security.CheakPassword();
-        calculator.AskVariables();
-        calculator.SolveWithSwitch();
-        calculator.SolveWithSwitchExpression();
-        calculator.DisplayInequality();
-        project.TimesTable();    
+        try
+        {
+            Security security = new Security();
+            Calculator calculator = new Calculator();
+            Projects project = new Projects();
+            System.Console.WriteLine("Program was created at :"+ DateTimeOffset.UtcNow);
+            security.CheakPassword();
+            calculator.AskVariables();
+            calculator.SolveWithSwitch();
+            calculator.SolveWithSwitchExpression();
+            calculator.DisplayInequality();
+            project.TimesTable();
+        }
+        catch(FormatException formatException)
+        {
+            System.Console.WriteLine($" Error detected: {formatException.Message}");
+        }
+        catch (Exception exception)
+        {
+            System.Console.WriteLine("Error was detected but type is unknown!"+ exception);
+        }
+        System.Console.WriteLine("Program ended...");
+            
     }
 }
